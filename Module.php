7d48ddac3fa2a674fb6b9975fbeb504f1c51ab8f
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-08-26 00:00:00 +0200

    Cleaned use of Generic module.

--- Module.php
+++ Module.php
@@ -28,7 +28,11 @@ namespace Thesaurus;
  * knowledge of the CeCILL license and that you accept its terms.
  */
 
-require_once dirname(__DIR__) . '/Generic/AbstractModule.php';
+if (!class_exists(\Generic\AbstractModule::class)) {
+    require file_exists(dirname(__DIR__) . '/Generic/AbstractModule.php')
+        ? dirname(__DIR__) . '/Generic/AbstractModule.php'
+        : __DIR__ . '/src/Generic/AbstractModule.php';
+}
 
 use Generic\AbstractModule;
 use Zend\ServiceManager\ServiceLocatorInterface;
@@ -55,7 +59,11 @@ class Module extends AbstractModule
 
     protected function installResources()
     {
-        require_once dirname(__DIR__) . '/Generic/InstallResources.php';
+        if (!class_exists(\Generic\InstallResources::class)) {
+            require_once file_exists(dirname(__DIR__) . '/Generic/InstallResources.php')
+                ? dirname(__DIR__) . '/Generic/InstallResources.php'
+                : __DIR__ . '/src/Generic/InstallResources.php';
+        }
 
         $services = $this->getServiceLocator();
         $installResources = new \Generic\InstallResources($services);
