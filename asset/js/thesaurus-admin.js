7890c212434be56a49758d47315d9e66aa3a1616
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-01-03 00:00:00 +0000

    Display link with title in structure.

--- asset/js/thesaurus-admin.js
+++ asset/js/thesaurus-admin.js
@@ -1,31 +1,83 @@
+/**
+ * Manage the case where Omeka is installed in a subdirectory.
+ */
+const basePath = window.location.pathname.substring(0, window.location.pathname.indexOf('/admin/')) + '/';
+
+/**
+ * Display element plugin for jsTree.
+ * Adapted from jstree-plugins.
+ */
+$.jstree.plugins.displayElement = function(options, parent) {
+   // Use a <i> instead of a <a> because inside a <a>.
+    var displayIcon = $('<i>', {
+        class: 'jstree-icon jstree-displaylink',
+        attr:{role: 'presentation'}
+    });
+    this.bind = function() {
+        parent.bind.call(this);
+        this.element.on(
+            'click.jstree',
+            '.jstree-displaylink',
+            $.proxy(function(e) {
+                var icon = $(e.currentTarget);
+                var node = icon.closest('.jstree-node');
+                var nodeObj = this.get_node(node);
+                var nodeUrl = nodeObj.data.url;
+                window.open(nodeUrl, '_blank');
+            }, this)
+        );
+    };
+    this.redraw_node = function(node, deep, is_callback, force_render) {
+        node = parent.redraw_node.apply(this, arguments);
+        if (node) {
+            var nodeObj = this.get_node(node);
+            var nodeUrl = nodeObj.data.url;
+            if (nodeUrl) {
+                var nodeJq = $(node);
+                var anchor = nodeJq.children('.jstree-anchor');
+                let anchorClone = displayIcon.clone();
+                anchorClone.attr('title', 'item #' + nodeObj.id);
+                anchor.append(anchorClone);
+            }
+        }
+        return node;
+    };
+};
+
 $(document).ready( function() {
 
 // Initialize the thesaurus tree.
 var tree = $('#jstree');
 var initialTreeData;
 
-// Add "data" to be be able to load core plugins.
+// Add "data" to be be able to load core plugins, and include item url.
 tree.data('jstree-data')
     .forEach(function(element, index) {
-        this[index].data = {};
+        this[index].data = {
+            url: basePath + 'admin/item/' + element.id,
+        };
     }, tree.data('jstree-data'));
 
-var jstree = tree.jstree({
-    'core': {
-        'check_callback': true,
-        'force_text': true,
-        'data': tree.data('jstree-data'),
-    },
-    'plugins': ['dnd', 'removenode', /* 'editlink' */, 'display']
-}).on('loaded.jstree', function() {
-    // Open all nodes by default.
-    tree.jstree(true).open_all();
-    initialTreeData = JSON.stringify(tree.jstree(true).get_json());
-}).on('move_node.jstree', function(e, data) {
-    // Open node after moving it.
-    var parent = tree.jstree(true).get_node(data.parent);
-    tree.jstree(true).open_all(parent);
-});
+var jstree = tree
+    .jstree({
+        'core': {
+            'check_callback': true,
+            'force_text': true,
+            'data': tree.data('jstree-data'),
+        },
+        // Plugins jstree and omeka (jstree-plugins).
+        'plugins': ['dnd', 'removenode', /*'editlink'*/, 'displayElement']
+    })
+    .on('loaded.jstree', function() {
+        // Open all nodes by default.
+        tree.jstree(true).open_all();
+        initialTreeData = JSON.stringify(tree.jstree(true).get_json());
+    })
+    .on('move_node.jstree', function(e, data) {
+        // Open node after moving it.
+        var parent = tree.jstree(true).get_node(data.parent);
+        tree.jstree(true).open_all(parent);
+    });
 
 $('#thesaurus-tree-form')
     .on('o:before-form-unload', function () {
--- src/Service/Controller/Admin/ThesaurusControllerFactory.php
+++ src/Service/Controller/Admin/ThesaurusControllerFactory.php
@@ -10,6 +10,7 @@ class ThesaurusControllerFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
+        // Required only because the controller extends Omeka ItemController for now.
         return new ThesaurusController(
             $services->get('Omeka\Media\Ingester\Manager')
         );
