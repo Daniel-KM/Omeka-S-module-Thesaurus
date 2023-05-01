b8833f1d6b3b11b12e2b6809cf98759820ab233b
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-05-01 00:00:00 +0000

    Released version 3.4.9.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -11,12 +11,14 @@ use Omeka\Stdlib\Message;
  * @var string $oldVersion
  *
  * @var \Omeka\Api\Manager $api
+ * @var \Omeka\View\Helper\Url $url
  * @var \Omeka\Settings\Settings $settings
  * @var \Doctrine\DBAL\Connection $connection
  * @var \Doctrine\ORM\EntityManager $entityManager
  * @var \Omeka\Mvc\Controller\Plugin\Messenger $messenger
  */
 $plugins = $services->get('ControllerPluginManager');
+$url = $services->get('ViewHelperManager')->get('url');
 $api = $plugins->get('api');
 $settings = $services->get('Omeka\Settings');
 $connection = $services->get('Omeka\Connection');
@@ -55,5 +57,31 @@ if (version_compare($oldVersion, '3.3.8.0', '<')) {
     $message = new Message(
         'It is now possible to get thesaurus data for another item without rebuilding it.' // @translate
     );
-    $messenger->addWarning($message);
+    $messenger->addSuccess($message);
+}
+
+if (version_compare($oldVersion, '3.4.9', '<')) {
+    $settings->set('thesaurus_property_descriptor', 'skos:prefLabel');
+    $settings->set('thesaurus_property_path', '');
+    $settings->set('thesaurus_property_ascendance', '');
+    $settings->set('thesaurus_separator', ' :: ');
+    $settings->set('thesaurus_select_display', 'ascendance');
+
+    $message = new Message(
+        'Many performance improvements have been implemented. Big thesaurus can be managed instantly.' // @translate
+    );
+    $messenger->addSuccess($message);
+
+    $message = new Message(
+        'It is now possible to import a file and to create a standard thesaurus with all linked resources.' // @translate
+    );
+    $messenger->addSuccess($message);
+
+    $settings->set('easyadmin_interface', ['resource_public_view']);
+    $message = new Message('%1$sNew setttings%2$s allow to store the path or the ascendance of each concept automatically or via the update button of the thesaurus.', // @translate
+        sprintf('<a href="%s">', $url('admin/default', ['controller' => 'setting'], ['fragment' => 'thesaurus'])),
+        '</a>'
+    );
+    $message->setEscapeHtml(false);
+    $messenger->addSuccess($message);
 }
